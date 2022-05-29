c801b6f0294ab61ccfb4a9b5eb7f216ae52b6463
ngotray <ngodinhtri@outlook.com>
2022-05-29 23:39:00 +0700

    dark theme

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,24 @@
+
 const sideMenu = document.getElementById("aside");
 const menuBtn = document.getElementById("menu-btn");
 const closeBtn = document.getElementById("close-btn");
+const themeToggle = document.getElementById("theme-toggle");
 
 menuBtn.onclick = () => {
     sideMenu.dataset.expanded = "true";
 }
 
 closeBtn.onclick = () => {
-     sideMenu.dataset.expanded = "false"
+    sideMenu.dataset.expanded = "false"
+}
+
+themeToggle.onclick = () => {
+    if (document.body.dataset.darkTheme == "true") {
+        document.body.dataset.darkTheme = "false"
+    } else {
+        document.body.dataset.darkTheme = "true"
+    }
+
+    themeToggle.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggle.querySelector('span:nth-child(2)').classList.toggle('active');
 }
